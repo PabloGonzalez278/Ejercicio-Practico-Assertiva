f6ba1aa5fa9cec5ed4b30ba364a3d4a8298b3939
Pablo Gonzalez <p-gonzalez@javeriana.edu.co>
2026-02-25 10:31:16 -0500

    inicio 2

---
+++ Contratistas/src/main/java/com/ejercicio/Contratistas/ContratistasApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio.Contratistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratistasApplication.class, args);
+	}
+
+}
