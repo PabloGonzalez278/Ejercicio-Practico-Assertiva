f3759727cef7f8deb53989a0450ede8a95966309
Pablo Gonzalez <p-gonzalez@javeriana.edu.co>
2026-03-02 17:57:11 -0500

    Update CorreoCorporativo.java

--- Contratistas/contratistas-csv/src/main/java/com/contratistas/contratistascsv/servicios/CorreoCorporativo.java
+++ Contratistas/contratistas-csv/src/main/java/com/contratistas/contratistascsv/servicios/CorreoCorporativo.java
@@ -1,5 +1,37 @@
 package com.contratistas.contratistascsv.servicios;
+import org.springframework.stereotype.Service;
+import java.text.Normalizer;
+import java.util.HashMap;
+import java.util.Map;
 
-
+@Service
 public class CorreoCorporativo {
+    private final Map<String, Integer> Contadorusuario = new HashMap<>();
+
+    public String Siguiente_Correo_Corporativo(String Nombre, String Dominio) {
+        String usuarioBase = UsuarioCorporativo(Nombre);
+        int contador = Contadorusuario.getOrDefault(usuarioBase, 0);
+        Contadorusuario.put(usuarioBase, contador + 1);
+        String usuario = contador == 0 ? usuarioBase: usuarioBase + contador;
+        return usuario + "@" + Dominio;
+    }
+
+    public String UsuarioCorporativo(String Nombre) {
+            String Normalizado = Normalizer.normalize(Nombre, Normalizer.Form.NFD).replaceAll("\\p{M}", "")
+                    .replaceAll("[^a-zA-Z\\s'-]", " ")
+                    .trim()
+                    .replaceAll("\\s+", " ");
+
+            String[] partes =Normalizado.split(" ");
+            if(partes.length < 2) {
+                throw new IllegalArgumentException("El nombre no es valido para crear el correo Corporativo" + Nombre);
+            }
+            String inicial = partes[0].substring(0,1).toLowerCase();
+            String apellido = partes[partes.length-1]
+                    .replace("'","")
+                    .replace("-","")
+                    .toLowerCase();
+            return inicial + apellido;
+    }
+
 }
