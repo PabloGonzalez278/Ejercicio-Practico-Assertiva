c3fe78e6086bda84deaa1cb11892edcbb01c5f97
Pablo Gonzalez <p-gonzalez@javeriana.edu.co>
2026-03-02 22:59:53 -0500

    actualizacion CSV

--- Contratistas/Python/app/main.py
+++ Contratistas/Python/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from app.core.proceso import run_process
+
 
 app = FastAPI()
 
@@ -7,3 +9,7 @@ app = FastAPI()
 def generate():
     result = run_process()
     return {"message": result}
+
+@app.get("/download")
+def download_csv():
+    return FileResponse("resultado/contratistas.csv", media_type="text/csv", filename="contratistas.csv")
